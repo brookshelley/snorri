c857b290cc46b2be4bc7c3d5019e99cbe1f4de18
Glitch (hello-express) <none>
2018-11-09 22:57:49 +0000

    👟💵 Checkpoint ./server.js:2204576/673

--- server.js
+++ server.js
@@ -11,28 +11,18 @@ const getYells = function () {
     return yells[Math.floor(Math.random() * yells.length)];
 }
 
-slack.on('/snorri', 'food', function (event) {
+slack.on('/snorri', function (event) {
     const response_url = event.response_url;
     slack.send(response_url, {
         text: getYells()
     })
 });
 
-slack.on('sleepies', function (event) {
+slack.on('food', function (event) {
     const response_url = event.response_url;
     slack.send(response_url, {
-        text: 'wowwwww'
+        text: getYells()
     })
-});
-
-slack.on('message', message => {
-  const response = messages.handleMessage(message.event.text);
-  if (response) {
-    slack.send({
-      text: response,
-      channel: message.event.channel
-    })
-  }
 });
 
 slack.on('*', event => { console.log(event) });
