8d0bfd45748d7281a86dd8d066d8fe820a7da37e
Glitch (hello-express) <none>
2018-11-10 00:24:49 +0000

    🗃🐢 Checkpoint ./server.js:2204576/115

--- server.js
+++ server.js
@@ -18,18 +18,21 @@ slack.on('/snorri', function (event) {
   if (event.text == 'food') {  
     const response_url = event.response_url;
     slack.send(response_url, {
+        response_type: "in_channel",
         text: getYells()
     })
   }
   else if (event.text == 'sleepies') {
     const response_url = event.response_url;
     slack.send(response_url, {
+        response_type: "in_channel",
         text: '_blinks_'
     })
   }
   else {
     const response_url = event.response_url;
     slack.send(response_url, {
+        response_type: "in_channel",
         text: '_stares at you_'
     })
   }
