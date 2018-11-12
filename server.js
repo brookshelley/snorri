b2683636390603f311c51afa38b9d6b8d78f3e84
Glitch (hello-express) <none>
2018-11-12 21:20:49 +0000

    🐣🌏 Checkpoint ./server.js:2204576/4315

--- server.js
+++ server.js
@@ -7,38 +7,10 @@ const dispatcher = require('httpdispatcher');
 const http = require('http');
 const EventEmitter = require('events');
 const app = express();
+var dispatcher = new HttpDispatcher();
 const slack = tinyspeck.instance({
     token: process.env.SLACK_ACCESS_TOKEN
 });
-
-class TinySpeck extends EventEmitter {
-  /**
-   * Contructor
-   *
-   * @param {Object} defaults - The default config for the instance
-   */
-  constructor(defaults) {
-    super();
-    this.cache = {};
-
-    // message defaults
-    this.defaults = defaults || {};
-    
-    // loggers
-    this.on('error', console.error);
-  }
-
-
-  /**
-   * Create an instance of the TinySpeck adapter
-   *
-   * @param {Object} defaults - The default config for the instance
-   * @return {TinySpeck} A new instance of the TinySpeck adapter
-   */
-  instance(defaults) {
-    return new this.constructor(defaults);
-  }
-}
 
 const yells = ['mreowww', 'wuoooahh', 'broo?', 'wooah', 'mwow?'];
 
@@ -72,41 +44,8 @@ slack.on('/snorri', function (event) {
 
 slack.on('*', event => { console.log(event) });
 
-slack.listen(process.env.PORT, process.env.SLACK_ACCESS_TOKEN);
-
-slack.listen(port, token) {
-// Display the Add to Slack button
-    dispatcher.onGet("/", function(req, res) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      let html = '<h1>Example Onboarding Slack Bot</h1><p>This project demonstrates how to build a Slack bot using Slack\'s Events API.</p><p>To test it out:</p><a id="add-to-slack" href="'+add_to_slack+'"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a><script src="https://button.glitch.me/button.js" data-style="glitch"></script><div class="glitchButton" style="position:fixed;top:20px;right:20px;"></div>';
-      res.end(html);
-    });     
-
-    return http.createServer((req, res) => {
-      let data = '';
-      
-      req.on('data', chunk => data += chunk);
-      
-      req.on('end', () => {
-        let message = this.parse(data);
-
-        // notify upon request
-        this.emit(req.url, message); 
-
-        // new subscription challenge
-        if (message.challenge){ console.log("verifying event subscription!"); res.end(message.challenge); return exit(); }
-        
-        // digest the incoming message
-        if (!token || token === message.token) this.digest(message);
-        
-        // close response
-        res.end();
-      });
-      
-      dispatcher.dispatch(req, res);
-    });
-};
-
+slack.listen(process.env.PORT, process.env.SLACK_ACCESS_TOKEN)
+             
 app.get('/auth', (req, res) =>{
     res.sendFile(__dirname + '/add_to_slack.html')
 })
