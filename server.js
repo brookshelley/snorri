ad3785b6822d0014572f8410b402d5d0826000a4
Glitch (hello-express) <none>
2018-11-14 01:10:56 +0000

    😭🌋 Checkpoint ./server.js:2204576/1382

--- server.js
+++ server.js
@@ -50,14 +50,14 @@ app.get('/auth', function(req, res) {
 
 app.get('/auth/redirect', (req, res) =>{
     var options = {
-        uri: 'https://slack.com/oauth/authorize'
+        uri: 'https://slack.com/api/oauth.access?code='
             +req.query.code+
             '&client_id='+process.env.CLIENT_ID+
             '&client_secret='+process.env.CLIENT_SECRET+
             '&redirect_uri='+process.env.REDIRECT_URI,
         method: 'GET'
     }
-    req(options, (error, response, body) => {
+    request(options, (error, response, body) => {
         var JSONresponse = JSON.parse(body)
         if (!JSONresponse.ok){
             console.log(JSONresponse)
@@ -67,4 +67,4 @@ app.get('/auth/redirect', (req, res) =>{
             res.send("Success!")
         }
     })
-}) 
+})
