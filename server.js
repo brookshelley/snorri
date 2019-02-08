bfe7fe3e73cc01d0a2f302e4b54461d3668ad12a
Glitch (hello-express) <none>
2019-02-08 00:25:59 +0000

    🌵🏟 Checkpoint ./server.js:2204576/1325

--- server.js
+++ server.js
@@ -37,7 +37,11 @@ slackEvents.on('link_shared', (event) => {
     user_auth_required: false,
     unfurls: {
       "https://www.brookshelley.com": {
-          //"text": "Every day is the test.",
+          "text": "Every day is the test.",
+          "title": "poop",
+          "author_name": "Bobby Tables",
+          //"author_link": "http://flickr.com/bobby/",
+          "author_icon": "https://brookshelley.com/IMG_0017%20copy.png"
           //blocks: [
             //{
               //"type": "image",
@@ -50,30 +54,6 @@ slackEvents.on('link_shared', (event) => {
               //"alt_text": "An incredibly cute kitten."
             //}  
         //]  
-          "attachments": [
-            {
-            "fallback": "Required plain-text summary of the attachment.",
-            "color": "#2eb886",
-            "pretext": "Optional text that appears above the attachment block",
-            "author_name": "Bobby Tables",
-            "author_link": "http://flickr.com/bobby/",
-            "author_icon": "http://flickr.com/icons/bobby.jpg",
-            "title": "Slack API Documentation",
-            "title_link": "https://api.slack.com/",
-            "text": "Optional text that appears within the attachment",
-            "fields": [
-                {
-                    "title": "Priority",
-                    "value": "High",
-                    "short": false
-                }
-            ],
-            "image_url": "http://my-website.com/path/to/image.jpg",
-            "thumb_url": "http://example.com/path/to/thumb.png",
-            "footer": "Slack API",
-            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-        }
-      ]
       }
     } 
   })
